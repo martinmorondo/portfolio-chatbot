db5992a2697ab0f5f864b6cf20f99b3732e625fa
Martín Morondo <martinmorondo@gmail.com>
2023-03-16 19:46:49 -0300

    Update TailwindCSS settings

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,tsx}",'./pages/**/*.tsx',
-  './components/**/*.tsx',
-  ],
+  content: ["./src/**/*.{html,js, jsx, tsx}",
+
+  '*.{html,js}'], //like this
+
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}

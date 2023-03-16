8f5c07ed0065a02a10f532c07eb7cb4849d42ab9
Martín Morondo <martinmorondo@gmail.com>
2023-03-16 20:30:59 -0300

    add new dependencies

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,tsx}",
-
-  '*.{html,js,jsx,tsx}'], //like this
-
+  mode: 'jit',
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
